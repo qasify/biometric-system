4cd7b9befa6387435125a182142cf335c49af56b
Muhammad Qasim <qasimirshad0@gmail.com>
2024-07-30 23:03:00 +0500

    button addition

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import React, { useEffect, useRef } from "react";
+import React, { } from "react";
 import Login from "./pages/Login";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ProtectedRoute from "./components/ProtectedRoute";
-// import { Button } from "./components";
+import { Button } from "./components";
 import useInstallPrompt from "./hooks/useInstallPrompt";
 
 const App: React.FC = () => {
   const promptInstall = useInstallPrompt();
-  const buttonRef = useRef<HTMLButtonElement>(null)
-
-  useEffect(() => {
-    // // Show install prompt on first load or after a certain event
-    // setTimeout(() => { promptInstall(); }, 2000)
-    if(buttonRef?.current){
-      // setTimeout(() => { buttonRef?.current?.click() }, 2000)
-    }
-  }, [promptInstall]);
 
   return (
     <div className="flex min-h-screen w-full overflow-y-auto bg-background-light">
@@ -27,7 +18,7 @@ const App: React.FC = () => {
         </Route>
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
-      {/* <Button onClick={()=>promptInstall()} className="fixed top-2 right-2 h-10">Install</Button> */}
+      <Button onClick={()=>promptInstall()} className="fixed top-2 right-2 h-10">Install</Button>
     </div>
   );
 };
